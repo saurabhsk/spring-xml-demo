f00b091f04b748ca547e3a91ac126add87fde04d
saurabhsk <saurabhkumar4634@gmail.com>
2019-03-01 15:23:36 +0530

    Task 3 files uploaded

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -10,6 +10,12 @@
 // with name having two values, like this:
 //<bean name=”MovieA, MovieB” ........> Update the code in Main to get the Movie bean by its two different
 // name.
+//Task 3: From the constructor-injection branch of spring-xml-demo repo create a autowire-xml branch.
+// For the Movie bean, delete the constructor based object injection in the bean definition file
+// (beans.xml) that injects an Actor bean. Use autowire byName in the Movie bean to inject an Actor
+// bean. Run the application. Create another Movie bean and try autowire byType. Run the application
+// and note the exception thrown. Fix the Movie bean by removing autowire byType and using constructor
+// injection instead
 package com.stackroute;
 
 import com.stackroute.domain.Actor;
