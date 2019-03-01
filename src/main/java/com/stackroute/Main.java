64121ca234fa72c5f85d6d650e165b2ccf82c139
saurabhsk <saurabhkumar4634@gmail.com>
2019-03-01 12:36:15 +0530

    Task 2 files uploaded

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,12 +1,15 @@
-//Task 1:Create a Maven project and add required dependency of spring-context5.1.4.RELEASECreate a Main
-// class in package com.stackroute and two Spring Beans –  Movie, and Actor in package com.stackroute.domain.
-// Actor has two String properties, name and gender, and an age property of type int.
-// An Actor can be initialized with the three properties via the corresponding setter methods.
-// Use property based injection in the bean definition file (beans.xml) Movie “has a” Actor that can be
-// initialized via the corresponding setter method. Use property based object injection in the bean
-// definition file (beans.xml) The Main class looks up Movie bean via three ways to print out actor
-// information: 1.Using XmlBeanFactory2.Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader3.
-// Using ApplicationContextCr
+//Task 2: From the master branch of spring-xml-demo repo create a constructor-injectionbranch.
+// Add constructor to the Actor class to initialize with name and gender, and ageCreate three beans of
+// type Actor in the bean definition file.  Use constructor-based injection in the bean definition file
+// (beans.xml) to inject property values in each of the three beans via name, index, and type respectively.
+// For the Movie bean, use constructor based object injection in the bean definition file (beans.xml) to
+// inject an Actor bean. In the Main class, look up Movie bean using ApplicationContext and print out Author
+// information. Use the same ApplicationContext to again look up the same Movie bean. Print out the equality
+// result of the two Movie beans. System.out.println(beanA==beanB); Change the scope of the Movie bean
+// in beans.xml to prototype and run the application again. Note the output. Replace id of the Movie bean
+// with name having two values, like this:
+//<bean name=”MovieA, MovieB” ........> Update the code in Main to get the Movie bean by its two different
+// name.
 package com.stackroute;
 
 import com.stackroute.domain.Actor;
@@ -26,29 +29,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //xmlBeanFactory method
-        Resource resource = new ClassPathResource("beans.xml");
-        XmlBeanFactory factory = new XmlBeanFactory(resource);
-
-        Actor actor= (Actor) factory.getBean("act");
-        System.out.println(actor);
-
-        //ApplicationContext Method
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        Actor actor1 = (Actor) context.getBean("act");
-        System.out.println(actor1);
-
-        //BeanDefinitionRegistry Method
-
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-
-        Actor actor2 = (Actor)beanFactory.getBean("act");
-        System.out.println(actor2);
+        Movie movie = (Movie) context.getBean("movieA");
+        System.out.println(movie);
 
 
+        Movie movie1 = (Movie) context.getBean("movieB");
+        if(movie.equals(movie1));{
+            System.out.println(movie==movie1);
+        }
+        Movie movie2 = (Movie) context.getBean("movieB");
+        System.out.println(movie2);
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,9 +2,22 @@ package com.stackroute.domain;
 
 public class Movie {
 
-    Actor actor;
+    Actor actor1;
+    Actor actor2;
+    Actor actor3;
 
-    public void setActor(Actor actor) {
-        this.actor = actor;
+    public Movie(Actor actor1, Actor actor2, Actor actor3) {
+        this.actor1 = actor1;
+        this.actor2 = actor2;
+        this.actor3 = actor3;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor1=" + actor1 +
+                ", actor2=" + actor2 +
+                ", actor3=" + actor3 +
+                '}';
     }
 }
