42fb6f4806e43f692ebc34c7e519a4c20c8ba865
saurabhsk <saurabhkumar4634@gmail.com>
2019-03-03 21:30:21 +0530

    Task 6 files uploaded

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,10 +1,7 @@
-//Task 5: From the aware-interface branch of spring-xml-demo repo create a bean-lifecyclebranch.
-// Add a BeanLifecycleDemoBean class in com.stackroute.demo that implements InitializingBean and DisposableBean.
-// Override the required methods to print out messages. Define BeanLifecycleDemoBean as a bean in beans.xml.
-// Run the application and observe the result.
-// Add two methods customInit() and customDestroy() to the  BeanLifecycleDemoBeanclass and print out
-// custom messages. In the BeanLifecycleDemoBean bean definition, in beans.xml, set the customInit()and
-// customDestroy() methods to be called.Run the application.
+//Task 6: From the bean-lifecycle branch of spring-xml-demo repo create a bean-post-processor branch.
+// Add a BeanPostProcessorDemoBean class in com.stackroute.demo that implements BeanPostProcessorOverride
+// the required methods to print out messages. Define BeanLifecycleDemoBean as a bean in beans.xml.
+// Run the application and observe the result
 package com.stackroute;
 
 import com.stackroute.demo.BeanLifecycleDemoBean;
@@ -31,7 +28,5 @@ public class Main {
         context.registerShutdownHook();
         Movie movie = (Movie) context.getBean("movieA");
         System.out.println(movie);
-
-        BeanLifecycleDemoBean bean= (BeanLifecycleDemoBean) context.getBean("BeanLifecycleDemoBean");
     }
 }
---
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,16 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException{
+        System.out.println("In After Initialization method.Bean name is "+ beanName);
+        return bean;
+    }
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException{
+        System.out.println("In Before Initialization method.Bean name is "+ beanName);
+        return bean;
+    }
+}
